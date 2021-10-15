00dfc1a043b95db073f08e3f561b08146c5081f7
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-10-15 15:51:16 +0200

    up to now

---
+++ javascript/script.js
@@ -0,0 +1,114 @@
+"use stict";
+
+//DOM OBJECTS
+
+
+const button = document.getElementById("run");
+
+
+button.addEventListener("click", function () {
+
+    const mainScreen = document.querySelector(".main-screen");
+    const pokeName = document.querySelector(".poke-name");
+    const pokeId = document.querySelector(".poke-id");
+    const pokeImg = document.querySelector(".poke-image");
+    const pokeMoveOne = document.querySelector(".poke-move-one");
+    const pokeMoveTwo = document.querySelector(".poke-move-two");
+    const pokeMoveThree = document.querySelector(".poke-move-three");
+    const pokeMoveFour = document.querySelector(".poke-move-four");
+    let input = document.getElementById("user-input").value;
+
+    fetch("https://pokeapi.co/api/v2/pokemon/" + input + "/")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+            pokeImg.src = data["sprites"]["front_default"];
+
+            // mainScreen.classList.remove("hide");
+            pokeName.textContent = data.name;
+            console.log(data.name);
+            pokeId.textContent = data["id"];
+
+
+            const dataMoves = data["moves"];
+            const dataFirstMove = dataMoves[0];
+            const dataSecondMove = dataMoves[1];
+            const dataThirdMove = dataMoves[2];
+            const dataForthMove = dataMoves[3];
+
+            // pokeMoveTwo.textContent = dataMoves[1]["move"]["name"];
+            // pokeMoveThree.textContent = dataMoves[2]["move"]["name"];
+            // pokeMoveFour.textContent = dataMoves[3]["move"]["name"];
+            if(dataFirstMove == false && dataSecondMove == false && dataSecondMove == false && dataThirdMove == false){
+                
+                pokeMoveOne.classList.remove("hide");
+                pokeMoveOne.innerHTML = "No Moves Yet";
+            }
+
+            if (dataFirstMove) {
+                pokeMoveOne.classList.remove("hide");
+                pokeMoveOne.textContent = dataFirstMove["move"]["name"];
+            } else {
+                pokeMoveOne.classList.add("hide");
+                pokeMoveOne.textContent = "";
+            }
+
+            if (dataSecondMove) {
+                pokeMoveTwo.classList.remove("hide");
+                pokeMoveTwo.textContent = dataSecondMove["move"]["name"];
+            } else {
+                pokeMoveTwo.classList.add("hide");
+                pokeMoveTwo.textContent = "";
+            }
+
+            if (dataThirdMove) {
+                pokeMoveThree.classList.remove("hide");
+                pokeMoveThree.textContent = dataThirdMove["move"]["name"];
+            } else {
+                pokeMoveThree.classList.add("hide");
+                pokeMoveThree.textContent = "";
+            }
+
+            if (dataForthMove) {
+                pokeMoveFour.classList.remove("hide");
+                pokeMoveFour.textContent = dataForthMove["move"]["name"];
+            } else {
+                pokeMoveFour.classList.add("hide");
+                pokeMoveFour.textContent = "";
+            }
+            console.log(data);
+        });
+
+        async function evoluctionChain(){
+            let evolutionChainAPI = await fetch ("https://pokeapi.co/api/v2/pokemon-species/" + input);
+            const data = await evolutionChainAPI.json();
+
+            const getDataFromChain = data.evolution_chain.url;
+            pokemonEvolution(getDataFromChain);
+        }
+        evoluctionChain();
+
+        async function pokemonEvolution(url){
+            let pokemonEvolutionAPI = await fetch(url);
+            const data = await pokemonEvolutionAPI.json();
+
+                let add = [];
+                let evolveTo= data.chain.evolves_to;
+
+                if(data.chain){
+                    add.push(data.chain.species.name);
+                    console.log(data);
+                }
+
+                for(let i=0; i<evolveTo.length; i++){
+                    if(evolveTo[i].evolve_to){
+                        for(let x=0; x<evolveTo[i].evolve_to.length; j++){
+                            add.push(evolveTo[i].evolve_to[j].species.name);
+                        }
+                    }
+                }    
+
+        }
+
+})
